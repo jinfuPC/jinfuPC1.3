0b72b0ccbf9c6a545243f6e3ca05014c9d0bc3cb
Y-Cwei <1095971737@qq.com>
2017-12-13 11:27:03 +0800

    合并投资管理

---
+++ App/Lib/Action/Member/InvestmentmanagementAction.class.php
@@ -0,0 +1,9 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+class InvestmentmanagementAction extends MCommonAction {
+
+       public function Investmentmanagement(){
+            $this->display("index");
+       }
+
+}
