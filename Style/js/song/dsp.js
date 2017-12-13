042e2bda4950e7eaf9af9ea73f9abe25895af47e
pc-PC\pc <solo_an0215@163.com>
2017-12-13 16:33:13 +0800

    金服右侧 返回顶部js

---
+++ Style/js/song/dsp.js
@@ -0,0 +1,13 @@
+var _adwq = _adwq || [];
+_adwq.push(['_setAccount', 'z9k3j']);
+_adwq.push(['_setDomainName', '.lichengdai.com']);
+_adwq.push(['_trackPageview']);
+
+(function() {
+var adw = document.createElement('script');
+adw.type = 'text/javascript';
+adw.async = true;
+adw.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://s') + '.emarbox.com/js/adw.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(adw, s);
+})();
